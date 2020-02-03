5238ca50aaafcb2a1dbc9519a35389a3d193ea25
joelethan <joelkatsme@gmail.com>
2020-02-03 11:50:33 -0800

    Connect books to mongoDB

---
+++ src/books.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const uri: string = 'mongodb://localhost:27017/mydb';
+
+mongoose.connect(uri, (err: any) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Successfully Connected to DB');
+    }
+})
+
+export const BookSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true }
+});
+
+const Book = mongoose.model('Book', BookSchema);
+export default Book;
